37a0b50e4243e26fbba6ea81ad7afe6f422c95a6
Leila rahou <LILIANLEILA365@ICLOUD.COM>
2024-01-31 22:16:09 +0100

    staged

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
- //le tableau
+//le tableau
 
   const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	},
+    {
+        "image":"slide1.jpg",
+        "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image":"slide2.jpg",
+        "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image":"slide3.jpg",
+        "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image":"slide4.png",
+        "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+    },
 ]
 //ETAPE 1:METTRE A JOUR LE CODE HTML
-let bannerImg = document.getElementById('.banner-img');
+let bannerImg = document.querySelector('.banner-img');
 let  arrowLeft = document.querySelector('.arrow_left');
 let arrowRight = document.querySelector('.arrow_right');
 let dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
@@ -28,12 +28,14 @@ let currentIndex = 0
 
 //ETAPE 2:AJOUTER UN ADDEVENTLISTENER SUR LES FLECHES
 //event listener sur la flèche de gauche. 
+// Gestionnaire d'événement pour le clic sur la flèche gauche
     arrowLeft.addEventListener('click',()=> {
     currentIndex = (currentIndex - 1);
     updateCarousel(currentIndex, 'left')
     updateDots(currentIndex); // Mettez à jour les points indicateurs
 });
 //event listener sur la flèche de droite. 
+// Gestionnaire d'événement pour le clic sur la flèche droite
 arrowRight.addEventListener('click',()=> {
     currentIndex = (currentIndex + 1) 
     updateCarousel(currentIndex, 'right')
@@ -42,7 +44,7 @@ arrowRight.addEventListener('click',()=> {
 
 //ETAPE 3:AJOUTER DES BULLET POINTS AU SLIDER
 //la condition pour mettre à jour les points indicateurs
-	
+    
 function updateDots(index) {
     dots.forEach((dot, i) => {
         if (i === index) {
@@ -70,18 +72,24 @@ function updateCarousel(index, direction) {
 
   // insérez la tagLine de la slide.
     let tagLine = slides[currentIndex].tagLine
-	  // Mettre à jour le texte 	//intégrer le texte avec innerHTML 
-      document.querySelector('p').innerHTML = tagLine
-      console.log(`Clic sur la flèche ${direction}`)
+      // Mettre à jour le texte     //intégrer le texte avec innerHTML 
+    document.querySelector('p').innerHTML = tagLine
+
+    console.log(`Clic sur la flèche ${direction}`)
 }
+
 
 // Afficher la première diapositive au chargement de la page
 updateCarousel(currentIndex, 'démarrage')
 updateDots(currentIndex) // Mettez à jour les points indicateurs pour la première diapositive
 
 
+
+
+
+   
     
-	
+    
 
  
 
